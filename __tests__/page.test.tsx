ff8fb08b5492bc358c24d438a74b6228062c7078
Jake Marchant <JSMarchant97@gmail.com>
2025-06-22 16:51:36 +0100

    Build: add Jest and general tidy up

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import Home from '../page'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+    
+    const heading = screen.getByRole('heading', {
+      name: /welcome to next.js/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+  it('renders a link to about page', () => {
+    render(<Home />)
+    
+    const link = screen.getByRole('link', {
+      name: /about/i,
+    })
+
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', '/about')
+  })
+}) 
--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Enable fast refresh in development
+    if (dev && !isServer) {
+      config.optimization.moduleIds = 'named'
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig 
